094efcf5636f4f9057f0732a1c619d73c8e30581
Galek <galek1357@gmail.com>
2024-02-01 15:13:00 +0600

    Lab3

--- Lab3/1.1function.py
+++ Lab3/1.1function.py
@@ -5,6 +5,22 @@ Create a function to convert grams to ounces.
 ounces = 28.3495231 * grams
 """
 def gramsToOunce(grams):
-    return 28.3495231 * grams
-x = int(input("Grams = "))
-print(f"Ounces = {gramsToOunce(x)}")
+    return grams / 28.3495231
+def ouncesToGrams(ounces):
+    return ounces * 28.3495231
+
+print("To convert from grams to ounces, input command 1 and click enter")
+print("To convert from ounces to gram, input command 2 and click enter")
+
+x = int(input())
+
+if x != 1 and x != 2:
+ print("Wrong command, use command 1 or 2 and nothing else!")
+ exit()
+elif x == 1:
+ y = float(input("Grams to ounces:"))
+ print(f"{y} grams = {gramsToOunce(y)} ounces") 
+elif x == 2:
+ y = float(input("Ounces to grams: "))
+ print(f"{y} ounces = {ouncesToGrams(y)} grams")
+ 
--- Lab3/1.2function.py
+++ Lab3/1.2function.py
@@ -3,20 +3,22 @@ Read in a Fahrenheit temperature.
 Calculate and display the equivalent centigrade temperature.
 The following formula is used for the conversion: C = (5 / 9) * (F - 32)
 """
+def CelsiustToFahrenheit(y):
+    return y * 9 / 5 + 32
+def FahrenheitToCelsius(y):
+    return (y - 32) * 5 / 9
 print("To convert from Celsius to Fahrenheit, input command 1 and click enter")
 print("To convert from Fahrenheit to Celsius, input command 2 and click enter")
 
 x = int(input())
 
 if x != 1 and x != 2:
- print("Wrong command, use command 1 or 2 and nothing else!")
- exit()
+    print("Wrong command, use command 1 or 2 and nothing else!")
+    exit()
 elif x == 1:
- y = float(input())
- z = y * 9 / 5 + 32
- print(f"Celsius to Fahrenheit: {y} -> {z}") 
+    y = float(input())
+    print(f"Celsius to Fahrenheit: {y} -> {CelsiustToFahrenheit(y)}") 
 elif x == 2: 
- y = float(input())
- z = (y - 32) * 5 / 9
- print(f"Fahrenheit to Celsius: {y} -> {z}")
+    y = float(input())
+    print(f"Fahrenheit to Celsius: {y} -> {FahrenheitToCelsius(y)}")
  
