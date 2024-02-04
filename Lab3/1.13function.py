37edcda522be04438942d3d8b9bbab67defdfd0b
Galek <galek1357@gmail.com>
2024-02-04 16:28:04 +0600

    Lab 3

--- Lab3/1.13function.py
+++ Lab3/1.13function.py
@@ -18,26 +18,29 @@ Take a guess.
 Good job, KBTU! You guessed my number in 3 guesses!
 """
 import random
-guess_times = 1
+
+def play_guessing_game(k):
+    guess_times = 1
+    
+    num = random.randint(1, k)  
+    
+    guess = int(input("Take a guess.\n"))
+
+    found = bool(num != guess) 
+    
+    while found: 
+        if guess < num:
+            guess_times += 1
+            guess = int(input("Your guess is too low.\nTake a guess.\n"))
+        elif guess > num:
+            guess_times += 1
+            guess = int(input("Your guess is too high.\nTake a guess.\n"))
+        else:
+            break
+    print(f"Good job, {name}! You guessed my number in {guess_times} guesses!")
 
 name = str(input("Hello! What is your name?\n"))
 k = int(input(f"Let's play \"Guess the number\"\n{name}, choose a range of numbers from 1 to k (k must be big for more fun)\nPlease, enter positive integer k = "))
 
 print(f"Well, {name}, I am thinking of a number between 1 and {k}.")
-
-num = random.randint(1, k)  
-
-guess = int(input("Take a guess.\n"))
-
-found = bool(num != guess) 
-while found: 
-    if guess < num:
-        guess_times += 1
-        guess = int(input("Your guess is too low.\nTake a guess.\n"))
-    elif guess > num:
-        guess_times += 1
-        guess = int(input("Your guess is too high.\nTake a guess.\n"))
-    else:
-        break
-
-print(f"Good job, {name}! You guessed my number in {guess_times} guesses!")
+play_guessing_game(k)
--- Lab3/1.5function.py
+++ Lab3/1.5function.py
@@ -3,9 +3,11 @@ Write a function that accepts string from user and print all permutations of tha
 """
 from itertools import permutations
 
-string = str(input())
-all_permutations = permutations(string)
+def permutation(string):
+    all_permutations = permutations(string)
+    for perm in all_permutations:
+        perm_str = ''.join(perm)
+        print(perm_str)
 
-for perm in all_permutations:
-    perm_str = ''.join(perm)
-    print(perm_str)
+string = str(input())
+permutation(string)
